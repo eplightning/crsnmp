80608a70d6b3c7872beca6cb68700e34ba75743b
Bartosz Sławianowski <eplightning@outlook.com>
2017-12-05 00:15:52 +0100

    Funkcja do wyświetlania binarnie ładnie

---
+++ src/debug/bin.cr
@@ -0,0 +1,13 @@
+module CrSNMP::Debug
+
+  def self.print_binary(arr : Array(UInt8))
+    arr.each do |i|
+      bitstring = (i.to_s 2)
+
+      pad = "0" * (8 - bitstring.size)
+
+      puts pad + bitstring
+    end
+  end
+
+end
