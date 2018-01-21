f0a13e619f6a7680d09b509c56c2dff6088a3097
Bartosz Sławianowski <eplightning@outlook.com>
2018-01-21 17:06:59 +0100

    Supports

--- src/data_source.cr
+++ src/data_source.cr
@@ -6,8 +6,9 @@ include CrSNMP::Shared
 module CrSNMP
 
   abstract class DataSource
-    abstract def get(oid : OID) : BER::DataValue | Nil
-    abstract def set(oid : OID, value : BER::DataValue) : Bool
+    abstract def get(oid : OID) : BER::DataValue
+    abstract def set(oid : OID, value : BER::DataValue)
+    abstract def supports(oid : OID) : Bool
   end
 
   class DataManager
